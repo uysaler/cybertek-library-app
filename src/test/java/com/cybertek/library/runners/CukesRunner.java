2824997d56335363f11ce9b8fcb28fcc831465e3
uysaler <aysepernur@gmail.com>
2021-03-03 13:47:37 -0600

    2

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html"},
+        plugin = {"html:target/cucumber-report.html",
+        "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions",
         dryRun = true,
